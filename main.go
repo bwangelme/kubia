848cd144f323f24d8760d18793932e4f5342826f
xuyundong <xuyundong@douban.com>
2020-04-01 22:04:22 +0800

    Feat: add unhealthy version

--- main.go
+++ main.go
@@ -7,7 +7,16 @@ import (
 	"os"
 )
 
+var count = 0
+
 func Home(w http.ResponseWriter, r *http.Request) {
+	count += 1
+	if count >= 5 {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "You got it, %d.\n", count)
+		return
+	}
+
 	fmt.Printf("Receive Request from %s\n", r.Host)
 	hostname, _ := os.Hostname()
 	fmt.Fprintf(w, "You've hit %s\n", hostname)
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # build
-docker build -t kubia:v0.1 .
+docker build -t bwangel/kubia:v0.2 .
 
 # run
-docker run -d -p 8080:8080 --name kubia-container kubia:v0.1
+docker run -d -p 8080:8080 --name kubia-container bwangel/kubia:v0.2
